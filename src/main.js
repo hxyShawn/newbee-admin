1f711ec0915a246b8de41d88edbd50d0a4b81816
hxyShawn <15868154378@163.com>
2021-07-01 22:56:24 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from '@/router'
+
+const app = createApp(App) // 生成 Vue 实例 app
+
+app.use(router) // 引用路由实例
+
+app.mount('#app') // 挂载到 #app
